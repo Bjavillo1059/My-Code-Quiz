585786389fb11a448ac3d5a66fe70be70836c365
Brandon Javillo <bjavillo1059@gmail.com>
2021-07-11 22:23:24 -0700

    added index, css, js

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # My-Code-Quiz
+
+# Description
+
+This code quiz was created for a quick snapshot of how much coding terms you can retain from the first few weeks of being in a Full Stack coding bootcamp.  The quiz is on a timer so you may or may not have enough time to look the answers up.  Please enjoy as this coding quiz was made with tlc, as well as, frustration and total stubborness.
+
---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+var questions = [
+    {   banner: "Arrays in Javascript can be used to store ____.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        rightAnswer: "all of the above"
+    },
+    {
+        banner: "When writing an array for a variable, what do you enclose the array with?"
+        choices: ["curly brackets", "parenthesis", "quotes", ]
+    }
+]
